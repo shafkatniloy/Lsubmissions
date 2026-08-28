f0208263c7affd1518efc51d0462853ebe58d35f
Safkat Islam Niloy <164136080+shafkatniloy@users.noreply.github.com>
2026-08-28 13:26:45 +0600

    Time: 123 ms (19.8%), Space: 142.2 MB (5.29%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -7,12 +7,16 @@ public:
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++)
         {
-            
-            if(mp.find(nums[i])!=mp.end())
-            {
-                if(abs(mp[nums[i]]-i) <= k) return true;
+            if(mp.find(nums[i])==mp.end()){
+                 mp[nums[i]]=i;
             }
-            mp[nums[i]]=i;
+            
+            else if(abs(i-mp[nums[i]])<=k) {
+                return true;
+            }
+            else{
+                mp[nums[i]]=i;
+            }
         }
         return false;
 
