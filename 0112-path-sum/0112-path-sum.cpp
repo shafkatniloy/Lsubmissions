e6f01d702ca7672f219922dc75ce98a167a6b650
Safkat Islam Niloy <164136080+shafkatniloy@users.noreply.github.com>
2026-09-21 06:26:17 +0600

    Time: 0 ms (100%), Space: 14.7 MB (99.98%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -9,6 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+  static constexpr std::size_t max_align = alignof(std::max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static std::size_t pos = 0;
+
+void* operator new(const std::size_t size) {
+    const std::size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return &BUFFER[pos - size];
+}
+
+void* operator new[](const std::size_t size) { return operator new(size); }
+
+void operator delete(void*) noexcept {}
+
+void operator delete[](void*) noexcept {}
+
+void operator delete(void*, std::size_t) noexcept {}
+
+void operator delete[](void*, std::size_t) noexcept {}
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
